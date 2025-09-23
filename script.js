c465fba45c61e1f626c886ef090ec0eadd1a4902
zibaakord <zibaaakord@gmail.com>
2025-09-23 15:16:07 +0330

    Implement dark/light mode toggle and enhance preformatted text styling

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function toggleSubmenu(button) {
 
   parent.classList.toggle('open');
 }
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkmode', 'inactive');
+}
+
+let darkMode = localStorage.getItem('darkmode');
+
+if (darkMode === "active") {
+  enableDarkMode();
+}
+
+const modeSwitchBtn = document.querySelector('.mode-switcher-button');
+
+modeSwitchBtn.addEventListener("click", () => {
+  darkMode = localStorage.getItem('darkmode');
+  darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+});
